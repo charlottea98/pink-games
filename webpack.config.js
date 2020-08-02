8f6c3b3d336fb573d2dd6dfe9522611cb366890a
Charlotte Andersson <charlotteandersson6@gmail.com>
2020-08-02 11:13:04 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] }
+    ]
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Awesome games"
+    })
+  ],
+  devServer: {
+    contentBase: "./dist"
+  }
+};
